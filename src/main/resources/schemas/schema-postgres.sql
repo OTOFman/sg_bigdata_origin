ecc44286dcfac9f563f8d1ce7654d81acacffcf3
OTOFman <zryu@1314-edu.com>
2019-03-22 09:41:02 +0800

    change init schema --story=1000265 --user=OTOFman

--- src/main/resources/schemas/schema-postgres.sql
+++ src/main/resources/schemas/schema-postgres.sql
@@ -2,6 +2,7 @@
 -- CREATE TABLE CommunityInfo(id serial PRIMARY KEY,
 --                         community_name VARCHAR(100),
 --                         build_year VARCHAR(100),
---                         building_amount VARCHAR(100),
---                         apartment_amount VARCHAR(100),
---                         price VARCHAR(100));
+--                         building_amount INTEGER ,
+--                         apartment_amount INTEGER ,
+--                         price INTEGER)
+create table IF NOT EXISTS TempCommunitiesName (id serial PRIMARY KEY, name varchar(100))
