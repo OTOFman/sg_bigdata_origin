9d71250fe98a41de92c4824d81323bbfecf6efa1
OTOFman <zryu@1314-edu.com>
2019-04-11 14:10:53 +0800

    render radar and import semantic ui frame --story=1000304 --user=OTOFman

---
+++ src/main/resources/static/js/site_selection.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    var ctx = document.getElementById('siteSelectionResult');
+    var myChart = new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: ['交通', '异业', '竞业'],
+            datasets: [radar_result]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+};
