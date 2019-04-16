7a929ac844693326f7fe5dea3e0710527ae2dade
OTOFman <zryu@1314-edu.com>
2019-04-16 16:09:22 +0800

    add automatic search and place search for map --story=1000304 --user=OTOFman

--- src/main/resources/static/js/site_selection.js
+++ src/main/resources/static/js/site_selection.js
@@ -1,6 +1,5 @@
 
 $.fn.loadMap = function () {
-    console.log('sasasa');
     var url = 'https://webapi.amap.com/maps?v=1.4.14&key=e70e70fc0c617d13aa771071fa154138&callback=onLoad';
     var jsapi = document.createElement('script');
     jsapi.charset = 'utf-8';
@@ -26,8 +25,39 @@ $.fn.getEvaluationRadarResult = function() {
     location.href = "/site_selection_radar?instituteName=" + document.getElementsByName("searchAddress")[0].value;
 };
 
+$.fn.searchPoiByname = function() {
+    var windowsArr = [];
+    var marker = [];
+    var map = new AMap.Map("auto_navi_map", {
+        resizeEnable: true,
+        center: [116.397428, 39.90923],//地图中心点
+        keyboardEnable: false
+    });
+    AMap.plugin(['AMap.Autocomplete','AMap.PlaceSearch'],function(){
+        var autoOptions = {
+            city: "武汉", //城市，默认全国
+            input: "tipinput"//使用联想输入的input的id
+        };
+        autocomplete= new AMap.Autocomplete(autoOptions);
+        var placeSearch = new AMap.PlaceSearch({
+            city:'武汉',
+            map:map
+        })
+        AMap.event.addListener(autocomplete, "select", function(e){
+            console.log(e.poi.name);
+            console.log(e.poi);
+            placeSearch.setCity(e.poi.adcode);
+            placeSearch.search(e.poi.name, function (status, result) {
+                console.log(status);
+                console.log(result);
+            })
+        });
+    });
+};
+
 $(document).ready(function(){
     $('.menu .item').tab();
+    $().searchPoiByname();
 });
 
 
