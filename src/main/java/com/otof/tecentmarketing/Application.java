fb5567dcc15b03d7ccb620fb196744966f5a512f
OTOFman <zryu@1314-edu.com>
2019-04-29 16:59:29 +0800

    rename class in application --story=1000304 --user=OTOFman

--- src/main/java/com/otof/tecentmarketing/Application.java
+++ src/main/java/com/otof/tecentmarketing/Application.java
@@ -1,10 +1,11 @@
 package com.otof.tecentmarketing;
 
-import com.otof.tecentmarketing.services.CommunityInfoService;
+import com.otof.tecentmarketing.factories.CrawlerFactory;
+import com.otof.tecentmarketing.services.CommunityStatisticService;
 import com.otof.tecentmarketing.services.HandleMapService;
+import com.otof.tecentmarketing.services.InitCrawlerService;
 import com.otof.tecentmarketing.services.JsoupCrawlerService;
 import com.otof.tecentmarketing.utils.GenerateMeituanSign;
-import org.apache.coyote.Request;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @MapperScan("com.otof.tecentmarketing.mapper")
@@ -29,7 +29,7 @@ public class Application {
     @Autowired
     private HandleMapService handleMapService;
     @Autowired
-    private CommunityInfoService communityInfoService;
+    private CommunityStatisticService communityStatisticService;
     @Autowired
     private GenerateMeituanSign generateMeituanSign;
 
